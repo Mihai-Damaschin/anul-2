f07fe6a23210e21074a67f86c9e99f6fa4324a04
Damaschin Mihai <mihai.damaschin@ebs-integrator.com>
2020-11-15 21:31:41 +0200

    Finished matrix problems

--- Str_de_date/my/laborator_4/prob_41.py
+++ Str_de_date/my/laborator_4/prob_41.py
@@ -15,55 +15,76 @@ for i in range(n):
         sorted_matrix[i][j] = a[k]
         k += 1
 
-# print("a)")
-# for i in range(n):
-#     for j in range(n):
-#         print('{} '.format(sorted_matrix[i][j if i % 2 == 0 else n - 1 - j]), end="")
-#     print()
-# print()
-#
-# print("b)")
-# b = [[0 for j in range(n)] for i in range(n)]
-# a_counter = 0
-# for k in range(n):
-#     i = 0
-#     while i <= k:
-#         if k % 2 == 0:
-#             b[n - (k - i) - 1][n - i - 1] = a[len(a) - a_counter - 1]
-#             b[k - i][i] = a[a_counter]
-#         else:
-#             if k != n - 1:
-#                 b[n - i - 1][n - (k - i) - 1] = a[len(a) - a_counter - 1]
-#             b[i][k - i] = a[a_counter]
-#         i += 1
-#         a_counter += 1
-#
-# for i in range(n):
-#     for j in range(n):
-#         print("{} ".format(b[i][j]), end="")
-#     print()
+print("\na)")
+for i in range(n):
+    for j in range(n):
+        print('{} '.format(sorted_matrix[i][j if i % 2 == 0 else n - 1 - j]), end="")
+    print()
 
-print("c)")
-c = [[0 for j in range(n)] for i in range(n)]
-new_n = n - 1
-sorted_item = 0
-for order_nr in range(n):
-    if order_nr % 4 == 0:
-        for i in range(n - order_nr):
-            # print("c[{}][{}] = a[{}]".format(order_nr, new_n, order_nr))
-            c[new_n - i][new_n] = a[sorted_item]
-            sorted_item += 1
-    if order_nr % 4 == 1:
-        for i in range(n - order_nr):
-            # print("c[{}][{}] = a[{}]".format(order_nr, new_n, order_nr))
-            c[new_n - i][new_n] = a[sorted_item]
-            sorted_item += 1
-
-print()
+print("\nb)")
+b = [[0 for j in range(n)] for i in range(n)]
+a_counter = 0
+for k in range(n):
+    i = 0
+    while i <= k:
+        if k % 2 == 0:
+            b[n - (k - i) - 1][n - i - 1] = a[len(a) - a_counter - 1]
+            b[k - i][i] = a[a_counter]
+        else:
+            if k != n - 1:
+                b[n - i - 1][n - (k - i) - 1] = a[len(a) - a_counter - 1]
+            b[i][k - i] = a[a_counter]
+        i += 1
+        a_counter += 1
 
 for i in range(n):
     for j in range(n):
-        if sorted_matrix[j][i] < 10:
+        if b[i][j] < 10:
+            print(" {} ".format(b[i][j]), end="")
+        else:
+            print("{} ".format(b[i][j]), end="")
+    print()
+
+c = [[0 for j in range(n)] for i in range(n)]
+new_n = n - 1
+sorted_item = 0
+i = new_n
+j = new_n
+
+for i in range(new_n, -1, -1):
+    c[i][j] = a[sorted_item]
+    sorted_item += 1
+
+for iteration in range(new_n - 1, -1, -1):
+    if iteration % 2 == 0:
+        for k  in range(iteration + 1):
+            j += 1
+            c[i][j] = a[sorted_item]
+            sorted_item += 1
+        for k  in range(iteration + 1):
+            i -= 1
+            if i < 0:
+                i += 1
+                break
+            c[i][j] = a[sorted_item]
+            sorted_item += 1
+    if iteration % 2 == 1:
+        for k  in range(iteration + 1):
+            j -= 1
+            if j < 0:
+                j += 1
+                break
+            c[i][j] = a[sorted_item]
+            sorted_item += 1
+        for k  in range(iteration + 1):
+            i += 1
+            c[i][j] = a[sorted_item]
+            sorted_item += 1
+
+print("\nc)")
+for i in range(n):
+    for j in range(n):
+        if c[i][j] < 10:
             print(" {} ".format(c[i][j]), end="")
         else:
             print("{} ".format(c[i][j]), end="")
